63b39038ee14d488ab0f296c90e99fb3e8f97f28
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2023-11-02 17:28:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # This entrypoint file to be used in development. Start by reading README.md
-from pytest import main
-
 from arithmetic_arranger import arithmetic_arranger
+from unittest import main
 
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
 # Run unit tests automatically
-main()
+main(module='test_module', exit=False)
